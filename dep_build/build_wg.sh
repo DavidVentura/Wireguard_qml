fc2b42e2f2c6d43034f836142e8a1855858a38e5
david <davidventura27@gmail.com>
2021-09-06 18:31:27 +0200

    update build steps for `wg`

--- dep_build/build_wg.sh
+++ dep_build/build_wg.sh
@@ -1,3 +1,12 @@
+docker buildx build --platform linux/armhf/v7 -t wgarmhf -f ./Dockerfile_wg .
 docker buildx build --platform linux/arm64/v8 -t wgaarch64 -f ./Dockerfile_wg .
-docker run -v $PWD/wireguard-tools/src:/data amd64 make -j
-docker run --platform linux/arm64/v8 -v $PWD/wireguard-tools/src:/data wgaarch64 make -j
+docker build -t wgamd64 -f ./Dockerfile_wg .
+
+docker run -v $PWD/wireguard-tools/src:/data wgamd64 make -j clean wg
+cp wireguard-tools/src/wg ../vendored/wg-x86_64
+
+docker run --platform linux/arm64/v8 -v $PWD/wireguard-tools/src:/data wgaarch64 make -j clean wg
+cp wireguard-tools/src/wg ../vendored/wg-arm64
+
+docker run --platform linux/armhf/v7 -v $PWD/wireguard-tools/src:/data wgarmhf make -j clean wg
+cp wireguard-tools/src/wg ../vendored/wg-arm
